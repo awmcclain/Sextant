ff8a0aa103c3411d1290bdb4a0fe583354940794
Andrew McClain <awmcclain@gmail.com>
2020-01-31 10:54:35 -0800

    Suggest take a detour when you select a long trip

--- Sextant.Domain/Commands/FSDTargetCommand.cs
+++ Sextant.Domain/Commands/FSDTargetCommand.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Stickymaddness All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using Sextant.Domain.Events;
 
 namespace Sextant.Domain.Commands
@@ -13,11 +14,13 @@ namespace Sextant.Domain.Commands
         private readonly IDetourPlanner _detourPlanner;
         private readonly INavigator _navigator;
         private readonly ILogger _logger;
+        private readonly ICommunicator _communicator;
 
-        public FSDTargetCommand(IDetourPlanner detourPlanner, INavigator navigator, ILogger logger)
+        public FSDTargetCommand(IDetourPlanner detourPlanner, INavigator navigator, ICommunicator communicator, ILogger logger)
         {
             _detourPlanner = detourPlanner;
             _navigator     = navigator;
+            _communicator  = communicator;
             _logger        = logger;
         }
 
@@ -25,6 +28,16 @@ namespace Sextant.Domain.Commands
         {
             if (_navigator.OnExpedition) {
                 return;
+            }
+
+            if (!_detourPlanner.DetourPlanned) {
+                _logger.Information("Checking to see if we have a long route plotted");
+                object val;
+                @event.Payload.TryGetValue("JumpsRemaining", out val);
+                int jumpsRemaining;
+                if (Int32.TryParse(val.ToString(), out jumpsRemaining) && jumpsRemaining > 5) {
+                    _communicator.Communicate("Commander, it looks as if you're going on a long route. If you'd like to scan high-value systems on the way, target your final destination and say 'take a detour'");
+                }
             }
 
             string location = @event.Payload["Name"].ToString();
